db8e541c2eafb48caa3ea6c173c9569f34fe5ffa
R4bsusail <silasbaur0@gmail.com>
2025-04-08 22:49:02 +0200

    java plugin to automatically update the via-version plugin

---
+++ Plugins/ViaVersionUpdater-21.1/src/main/java/com/grafando/viaVersionUpdater/UpdateVias.java
@@ -0,0 +1,148 @@
+package com.grafando.viaVersionUpdater;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import static com.grafando.viaVersionUpdater.BuildYml.getDownloadedBuild;
+import static com.grafando.viaVersionUpdater.BuildYml.updateBuildNumber;
+
+public class UpdateVias {
+    private static String name;
+    private static String directory;
+    private static String branch;
+
+    public static boolean updateVia(String viaName, String dataDirectory, boolean isDev, boolean isJava8) throws IOException {
+        name = viaName.replace("ViaRewind%20Legacy%20Support-Dev", "ViaRewind%20Legacy%20Support%20DEV");
+        directory = dataDirectory;
+
+        if (isDev && !isJava8) {
+            branch = "dev";
+        } else {
+            branch = "master";
+        }
+
+        if (getDownloadedBuild(name) == -1) {
+            downloadUpdate(name);
+            updateBuildNumber(name, getLatestBuild());
+            System.out.println(name + " was downloaded for the first time. Please restart to let the plugin take effect.");
+            return true;
+        } else if (getDownloadedBuild(name) != getLatestBuild()) {
+            downloadUpdate(name);
+            updateBuildNumber(name, getLatestBuild());
+            return true;
+        }
+        return false;
+    }
+
+    public static int getLatestBuild() throws IOException {
+        String jenkinsUrl = "https://ci.viaversion.com/job/" + name + "/lastSuccessfulBuild/api/json";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode node = objectMapper.readTree(new URL(jenkinsUrl));
+
+        JsonNode actions = node.get("actions");
+        if (actions == null || !actions.isArray()) {
+            System.err.println("Error: 'actions' array is missing or not an array.");
+            return -1;
+        }
+
+        JsonNode buildsByBranchName = null;
+        for (JsonNode action : actions) {
+            if (action != null && action.has("buildsByBranchName")) {
+                buildsByBranchName = action.get("buildsByBranchName");
+                break;
+            }
+        }
+
+        if (buildsByBranchName == null) {
+            System.err.println("Error: 'buildsByBranchName' field is missing in any 'actions' element.");
+            return -1;
+        }
+
+        // maybe its java8-build and not java8? change branch name
+        JsonNode branchNode = buildsByBranchName.get("refs/remotes/origin/" + branch);
+        if (branchNode == null) {
+            System.err.println("Error: Branch 'refs/remotes/origin/" + branch + "' is missing.");
+            return -1;
+        }
+
+        JsonNode buildNumberNode = branchNode.get("buildNumber");
+        if (buildNumberNode == null) {
+            System.err.println("Error: 'buildNumber' field is missing.");
+            return -1;
+        }
+
+        return buildNumberNode.asInt();
+    }
+
+    public static void downloadUpdate(String s) throws IOException {
+        String latestVersionUrl = "https://ci.viaversion.com/job/" + s + "/lastSuccessfulBuild/artifact/" + getLatestDownload(s);
+
+
+        boolean doesUpdateFolderExist = new File(directory, File.separator + "update").exists();
+        String outputFilePath = directory + "/" + s + ".jar";
+        if (doesUpdateFolderExist) {
+            File directoryFile = new File(directory);
+            File[] files = directoryFile.listFiles();
+            if (files != null) {
+                boolean containsVia = false;
+                for (File file : files) {
+                    if (!file.isDirectory() && file.getName().toLowerCase().contains(s.toLowerCase().replace("-dev", "").replace("%20", "-").replace("-java8", ""))) {
+                        containsVia = true;
+                        break;
+                    }
+                    /*if (file.getName().toLowerCase().contains("viaversion")) {
+                        containsVia = true;
+                        break;
+                    }*/
+                }
+                if (containsVia) {
+                    outputFilePath = directory + "/update/" + s + ".jar";
+                }
+            }
+        }
+
+        try (InputStream in = new URL(latestVersionUrl).openStream();
+             FileOutputStream out = new FileOutputStream(outputFilePath)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+            System.out.println("New version of " + s + " downloaded. Please restart the server.");
+        } catch (IOException e) {
+            System.out.println("Error downloading new version of " + s + "\n" + e);
+        }
+    }
+
+    public static String getLatestDownload(String s) throws IOException {
+        String jenkinsUrl = "https://ci.viaversion.com/job/" + s + "/lastSuccessfulBuild/api/json";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode node = objectMapper.readTree(new URL(jenkinsUrl));
+
+        ArrayNode artifacts = (ArrayNode) node.get("artifacts");
+
+        JsonNode selectedArtifact = null;
+        for (JsonNode artifact : artifacts) {
+            String fileName = artifact.get("fileName").asText();
+            if (!fileName.contains("sources")) {
+                selectedArtifact = artifact;
+                break;
+            }
+        }
+
+        if (selectedArtifact == null && !artifacts.isEmpty()) {
+            selectedArtifact = artifacts.get(0);
+        }
+
+        return selectedArtifact.get("relativePath").asText();
+    }
+}
---
+++ Plugins/ViaVersionUpdater-21.1/src/main/java/com/grafando/viaVersionUpdater/ViaVersionUpdater.java
@@ -0,0 +1,136 @@
+package com.grafando.viaVersionUpdater;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+
+import static com.grafando.viaVersionUpdater.BuildYml.createYamlFile;
+import static com.grafando.viaVersionUpdater.BuildYml.updateBuildNumber;
+import static com.grafando.viaVersionUpdater.UpdateVias.updateVia;
+
+public final class ViaVersionUpdater extends JavaPlugin {
+
+    private FileConfiguration config;
+    public boolean isViaVersionEnabled;
+    public boolean isViaVersionDev;
+    public boolean isViaVersionJava8;
+    public boolean isViaBackwardsEnabled;
+    public boolean isViaBackwardsDev;
+    public boolean isViaBackwardsJava8;
+    public boolean isViaRewindEnabled;
+    public boolean isViaRewindDev;
+    public boolean isViaRewindJava8;
+    public boolean isViaRewindLegacyEnabled;
+    public boolean isViaRewindLegacyDev;
+
+    @Override
+    public void onEnable() {
+        new Metrics(this, 18603);
+        loadConfiguration();
+        createYamlFile(getDataFolder().getAbsolutePath(), false);
+        isViaVersionEnabled = getConfig().getBoolean("ViaVersion.enabled");
+        isViaVersionDev = getConfig().getBoolean("ViaVersion.dev");
+        isViaVersionJava8 = getConfig().getBoolean("ViaVersion.java8");
+        isViaBackwardsEnabled = getConfig().getBoolean("ViaBackwards.enabled");
+        isViaBackwardsDev = getConfig().getBoolean("ViaBackwards.dev");
+        isViaBackwardsJava8 = getConfig().getBoolean("ViaBackwards.java8");
+        isViaRewindEnabled = getConfig().getBoolean("ViaRewind.enabled");
+        isViaRewindDev = getConfig().getBoolean("ViaRewind.dev");
+        isViaRewindJava8 = getConfig().getBoolean("ViaRewind.java8");
+        isViaRewindLegacyEnabled = getConfig().getBoolean("ViaRewind-Legacy.enabled");
+        isViaRewindLegacyDev = getConfig().getBoolean("ViaRewind-Legacy.dev");
+        updateChecker();
+        getCommand("updatevias").setExecutor(new UpdateCommand());
+
+    }
+
+    public void updateChecker() {
+        config = getConfig();
+        String cronExpression = config.getString("Cron-Expression", "");
+        long interval = config.getLong("Check-Interval");
+        long delay = config.getLong("Delay");
+
+        if (!cronExpression.isEmpty()) {
+            CronScheduler scheduler = new CronScheduler(cronExpression);
+            Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> scheduler.runIfDue(v -> checkUpdateVias()), delay * 20L, 20L);
+        } else {
+            Bukkit.getScheduler().runTaskTimerAsynchronously(this, this::checkUpdateVias, delay * 20L, 20L * 60L * interval);
+        }
+    }
+
+    public void checkUpdateVias(){
+        try {
+            if(Bukkit.getPluginManager().getPlugin("ViaVersion") == null){
+                updateBuildNumber("ViaVersion", -1);
+            }
+            if(Bukkit.getPluginManager().getPlugin("ViaBackwards") == null){
+                updateBuildNumber("ViaBackwards", -1);
+            }
+            if(Bukkit.getPluginManager().getPlugin("ViaRewind") == null){
+                updateBuildNumber("ViaRewind", -1);
+            }
+            if(Bukkit.getPluginManager().getPlugin("ViaRewind-Legacy-Support") == null){
+                updateBuildNumber("ViaRewind%20Legacy%20Support", -1);
+            }
+            if (isViaVersionEnabled) {
+                updateAndRestart("ViaVersion", isViaVersionDev, isViaVersionJava8);
+            }
+            if (isViaBackwardsEnabled) {
+                updateAndRestart("ViaBackwards", isViaBackwardsDev, isViaBackwardsJava8);
+            }
+            if (isViaRewindEnabled) {
+                updateAndRestart("ViaRewind", isViaRewindDev, isViaRewindJava8);
+            }
+            if (isViaRewindLegacyEnabled) {
+                updateAndRestart("ViaRewind%20Legacy%20Support", isViaRewindLegacyDev, false);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateAndRestart(String pluginName, boolean isDev, boolean isJava8) throws IOException {
+        String pluginKey = isJava8 ? pluginName + "-Java8" : (isDev ? pluginName + "-Dev" : pluginName);
+        if (updateVia(pluginKey, getDataFolder().getParent(), isDev, isJava8) && getConfig().getBoolean("AutoRestart")) {
+            Bukkit.broadcastMessage(config.getString("AutoRestart-Message"));
+            Bukkit.getScheduler().runTaskLater(this, Bukkit::shutdown, config.getLong("AutoRestart-Delay") * 20L);
+        }
+    }
+
+    public void loadConfiguration(){
+        saveDefaultConfig();
+
+        config = getConfig();
+        config.addDefault("ViaVersion.enabled", true);
+        config.addDefault("ViaVersion.dev", false);
+        config.addDefault("ViaBackwards.enabled", true);
+        config.addDefault("ViaBackwards.dev", false);
+        config.addDefault("ViaRewind.enabled", true);
+        config.addDefault("ViaRewind.dev", false);
+        config.addDefault("ViaRewind-Legacy.enabled", true);
+        config.addDefault("Check-Interval", 60);
+        config.options().copyDefaults(true);
+        saveConfig();
+    }
+
+    public class UpdateCommand implements CommandExecutor {
+
+        @Override
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+            checkUpdateVias();
+            sender.sendMessage(ChatColor.AQUA + "Update checker for vias successful!");
+            return true;
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
